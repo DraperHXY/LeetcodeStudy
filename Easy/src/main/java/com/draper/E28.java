511207ca26227108f886e51eeac37f58850d2add
draper_hxy <draper.hxy@gmail.com>
2019-08-30 22:11:50 +0800

    update -> E28

--- Easy/src/main/java/com/draper/E28.java
+++ Easy/src/main/java/com/draper/E28.java
@@ -10,24 +10,24 @@ public class E28 {
     private static String haystack1 = "hello", needle1 = "ll";
     private static String haystack2 = "aaaaa", needle2 = "bba";
     private static String haystack3 = null, needle3 = "";
-//    private static String haystack4 = "a", needle4 = "";
+    //    private static String haystack4 = "a", needle4 = "";
     private static String haystack5 = "", needle5 = "";
     private static String haystack6 = "mississippi", needle6 = "mississippi";
     private static String haystack7 = "a", needle7 = "a";
 
     public static void main(String[] args) {
-//        System.out.println(strStr(haystack1, needle1));
+        System.out.println(strStr(haystack1, needle1));
 //        System.out.println(strStr(haystack2, needle2));
 //        System.out.println(strStr(haystack3, needle3));
 //        System.out.println(strStr(haystack4, needle4));
 //        System.out.println(strStr(haystack5, needle5));
 //        System.out.println(strStr(haystack6, needle6));
-        System.out.println(strStr(haystack7, needle7));
+//        System.out.println(strStr(haystack7, needle7));
 
 
     }
 
-    public static int strStr(String haystack, String needle) {
+    public static int strStr0(String haystack, String needle) {
 
         if (needle.length() > haystack.length()) {
             return -1;
@@ -58,5 +58,29 @@ public class E28 {
 
 
         return -1;
+    }
+
+    // 别人的方法，感觉
+    public static int strStr(String haystack, String needle) {
+        int slen = needle.length();
+        int blen = haystack.length();
+
+        if (slen > blen) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = slen - 1;
+
+        while (end < blen) {
+            if (haystack.substring(start, end + 1).equals(needle)) {
+                return start;
+            }
+            start++;
+            end++;
+        }
+
+        return -1;
+
     }
 }
