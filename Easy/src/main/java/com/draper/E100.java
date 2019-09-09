9a54a3adbefd9fbdf9ae3434e113dc072470b3e1
draper_hxy <draper.hxy@gmail.com>
2019-09-09 23:35:50 +0800

    add -> E100

---
+++ Easy/src/main/java/com/draper/E100.java
@@ -0,0 +1,38 @@
+package com.draper;
+
+/**
+ * @author draper_hxy
+ */
+public class E100 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
