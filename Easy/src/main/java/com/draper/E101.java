4c1f40c1e92bb2e8a8dc236ffefa87723f025b6c
draper_hxy <draper.hxy@gmail.com>
2019-09-10 09:14:26 +0800

    add -> E101

---
+++ Easy/src/main/java/com/draper/E101.java
@@ -0,0 +1,36 @@
+package com.draper;
+
+/**
+ * @author draper_hxy
+ */
+public class E101 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirro(root.left, root.right);
+
+    }
+
+    public static boolean isMirro(TreeNode leftNode, TreeNode rightNode) {
+
+        if (leftNode == null && rightNode == null)
+            return true;
+
+        if (leftNode == null || rightNode == null)
+            return false;
+
+        if (leftNode.val != rightNode.val)
+            return false;
+
+
+        return isMirro(leftNode.left, rightNode.right) && isMirro(leftNode.right, rightNode.left);
+
+    }
+
+}
