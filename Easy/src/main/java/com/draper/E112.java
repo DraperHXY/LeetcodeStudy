af6ddc929f214061d6eee84f6d56369172eac417
draper_hxy <draper.hxy@gmail.com>
2019-09-16 23:29:12 +0800

    update -> E112

--- Easy/src/main/java/com/draper/E112.java
+++ Easy/src/main/java/com/draper/E112.java
@@ -3,23 +3,40 @@ package com.draper;
 /**
  * @author draper_hxy
  */
+@SuppressWarnings("all")
 public class E112 {
 
     public static void main(String[] args) {
-        System.out.println(hasPathSum(TreeUtil.convert(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1}), 22));
+        System.out.println(hasPathSum1(TreeUtil.convert(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1}), 22));
+        System.out.println(hasPathSum2(TreeUtil.convert(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1}), 22));
     }
 
-    public static boolean hasPathSum(TreeNode root, int sum) {
+    public static boolean hasPathSum1(TreeNode root, int sum) {
         if (root == null) {
             return false;
         }
 
         sum = sum - root.val;
-        if (root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             return sum == 0;
         }
 
-        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        return hasPathSum1(root.left, sum) || hasPathSum1(root.right, sum);
+    }
+
+    public static boolean hasPathSum2(TreeNode root, int sum) {
+        return search(root, sum, 0);
+    }
+
+    public static boolean search(TreeNode node, int sum, int hasSum) {
+        if (node == null) {
+            return false;
+        }
+        hasSum = hasSum + node.val;
+        if (node.left == null && node.right == null) {
+            return sum == hasSum;
+        }
+        return search(node.left, sum, hasSum) || search(node.right, sum, hasSum);
     }
 
 }
