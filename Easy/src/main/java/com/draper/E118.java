b30fec7e580fd33613a69167d152a423e7d6773a
draper_hxy <draper.hxy@gmail.com>
2019-09-18 00:03:21 +0800

    update -> E118

--- Easy/src/main/java/com/draper/E118.java
+++ Easy/src/main/java/com/draper/E118.java
@@ -10,24 +10,34 @@ import java.util.List;
  */
 public class E118 {
 
-    public List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
-        int[] lastArray = {1};
+        Integer[] lastArray = {1};
+
+
         for (int i = 1; i <= numRows; i++) {
             Integer[] childArray = new Integer[i];
             childArray[0] = lastArray[0];
             childArray[childArray.length - 1] = lastArray[lastArray.length - 1];
 
-            for (int i1 = 1; i1 < childArray.length - 1; i1++) {
-                childArray[i1] = lastArray[]
-            }
-
+            if (i != 1)
+                for (int i1 = 1; i1 < childArray.length - 1; i1++) {
+                    childArray[i1] = lastArray[i1 - 1] + lastArray[i1];
+                }
             list.add(Arrays.asList(childArray));
+            lastArray = childArray;
         }
+        return list;
     }
 
     public static void main(String[] args) {
-
+        List<List<Integer>> list = generate(5);
+        for (List<Integer> integerList : list) {
+            for (Integer integer : integerList) {
+                System.out.print(integer + "\t");
+            }
+            System.out.println();
+        }
     }
 
 }
