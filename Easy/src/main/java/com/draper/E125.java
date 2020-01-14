1b2108c29e0f72ed139cb150635ba5881deff2ed
draper_hxy <draper.hxy@gmail.com>
2020-01-14 09:40:24 +0800

    add -> E125

---
+++ Easy/src/main/java/com/draper/E125.java
@@ -0,0 +1,16 @@
+package com.draper;
+
+/**
+ * @author draper_hxy
+ */
+public class E125 {
+
+    public boolean isPalindrome(String s) {
+        if (s.equals("")) {
+            return true;
+        }
+        String ss = s.toLowerCase().replaceAll("[a-zA-Z0-9]", "");
+        return new StringBuilder(ss).reverse().toString().equals(ss);
+    }
+
+}
